e4f832e592b3d70847073929d247833924287162
Cheng Yichao <onesuperclark@gmail.com>
2014-10-23 23:59:14 +0800

    Add macro TRY to simulate exception

--- src/olive_util.h
+++ src/olive_util.h
@@ -3,13 +3,22 @@
  *
  * Author: Yichao Cheng (onesuperclark@gmail.com)
  * Created on: 2014-10-20
- * Last Modified: 2014-10-22
+ * Last Modified: 2014-10-23
  */
 
 #ifndef OLIVE_UTIL_H
 #define OLIVE_UTIL_H
 
 
+/**
+ * Simulates simple exceptions: if the statement is not correct, jump to
+ * label, typically an error label where you could handle the 'exception'
+ */
+#define TRY(stmt, label)               \
+  do {                                 \
+    if (!(stmt))                       \
+      goto label;                      \
+  } while (0);
 /**
  * Cuda runtime error checking. The cuda runtime errors will be treated as
  * fatal errors and will make the program exit.
@@ -29,12 +38,62 @@
             assert(0);                                              \
         }                                                           \ 
     } while (0);
-
 /**
  * CUT_CALL_SAFE is used to wrap around the cuda runtime call to 
  * check if it causes any error.
  */
 #define CUT_CALL_SAFE(func) { func; CUT_CHECK_ERROR(); }
+/**
+ * Timing info printer. Can be toggle off if wanted
+ */
+#ifdef OLIVE_TIMING
+    #define olive_tim(...)                      \
+        do {                                    \
+            fprintf(stdout, "Tim: ");           \
+            fprintf(stdout, __VA_ARGS__);       \
+            fprintf(stdout, "\n");              \
+            fflush(stdout);                     \
+        } while (0);                        
+#else
+    #define olive_tim(...)  // disregarded
+#endif
+/**
+ * Logging info printer. Can be toggle off if wanted
+ */
+#ifdef OLIVE_LOGGING
+    #define olive_log(...)                      \
+        do {                                    \
+            fprintf(stdout, "Log: ");           \
+            fprintf(stdout, __VA_ARGS__);       \
+            fprintf(stdout, "\n");              \
+            fflush(stdout);                     \
+        } while (0);
+#else
+    #define olive_log(...) // disregarded
+#endif
+/**
+ * Fatal error (can not be disregarded)
+ * Print the message and quit program immediately
+ */
+#define olive_fatal(...)                        \
+    do {                                        \
+        fprintf(stderr, "Fatal: ");             \
+        fprintf(stderr, __VA_ARGS__);           \
+        fprintf(stderr, "\n");                  \
+        fflush(stdout);                         \ 
+        assert(false);                          \
+    } while (0);
+/**
+ * Fatal error (can not be disregarded)
+ * Print the message and let the program handle it
+ */
+#define olive_error(...)                        \
+    do {                                        \
+        fprintf(stderr, "Error: ");             \
+        fprintf(stderr, __VA_ARGS__);           \
+        fprintf(stderr, "\n");                  \
+        fflush(stdout);                         \
+    } while (0);
 
 
 // @return the number of the GPUs
@@ -46,46 +105,28 @@ void set_gpu_num(int);
 // Check how much memory is available currently
 void check_available_memory(void);
 
-// initialize the timer before we can use time_elapsed() 
-void init_timer(void);
+/**
+ * Checks if the string is a numeric number
+ * @param[in] str: the string to check
+ * @return true if the string represents a numeric number
+ */
+inline bool is_numeric(char * str);
 
-// @return the current time 
-double get_time(void);
+// A simple timer
+// TODO: add more functions to make it more powerful
+class Timer {
+private:
+    double last_time;
+public:
+    // initialize the timer before we can use time_elapsed() 
+    void initialize(void);
 
-// @return the elapsed time between two continous call to this function
-double time_elapsed(void);
+    // @return the current time 
+    double get_time(void);
 
-
-// Timing info printer
-#ifdef OLIVE_TIMING
-    #define olive_tim(...) do {         \
-        fprintf(stdout, "Tim: ");       \
-        fprintf(stdout, __VA_ARGS__);   \
-        fprintf(stdout, "\n");          \
-        fflush(stdout); } while (0);    
-#else
-    #define olive_tim(...)  // disregarded
-#endif
-
-// Logging info printer
-#ifdef OLIVE_LOGGING
-    #define olive_log(...) do {         \
-        fprintf(stdout, "Log: ");       \
-        fprintf(stdout, __VA_ARGS__);   \
-        fprintf(stdout, "\n");          \
-        fflush(stdout); } while (0);
-#else
-    #define olive_log(...) // disregarded
-#endif
-
-// Fatal error (can not be disregarded)
-// Print the message and quit program immediately
-#define olive_fatal(...) do {            \
-        fprintf(stderr, "Error: ");      \
-        fprintf(stderr, __VA_ARGS__);    \
-        fprintf(stderr, "\n");           \
-        fflush(stdout);                  \ 
-        assert(false); } while (0);
+    // @return the elapsed time between two continous call to this function
+    double elapsed_time(void);    
+};
 
 
 #endif // OLIVE_UTIL_H
