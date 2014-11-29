f068eac12d1c4bcfa6f4006b489cac9c25839fa7
Cheng Yichao <onesuperclark@gmail.com>
2014-11-29 23:07:54 +0800

    [Feature] message copy function.

--- src/olive/message_box.h
+++ src/olive/message_box.h
@@ -9,6 +9,11 @@
 #ifndef MESSAGE_BOX_H
 #define MESSAGE_BOX_H
 
+
+
+/**
+ * MessageBox does not utilize GRD since the buffer only lives on GPU.
+ */
 class MessageBox {
  public:
     /**
@@ -21,48 +26,76 @@ class MessageBox {
         void *    message;   // Content of the message.
     };
 
+    Message * buffers[2];   /** Using a double-buffering method. */
+    int       deviceId;     /** Where the message box locates */
+    size_t    maxLength;    /** Maximum length of the buffer */
+    size_t    length;      /** Current capacity of the message box. */
+
     /**
-     * Ising a double-buffering method.
+     * Constructor. `deviceId < 0` if there is no memory reserved
      */
-    GRD<Message> * firstBuffer;
-    GRD<Message> * secondBuffer;
-
-    /** Current capacity of the message box */
-    size_t size;
-
-    // Where the message box locates
-    int deviceId;
-
-    /** Constructor */
-    MessageBox(): firstBuffer(NULL), secondBuffer(NULL), deviceId(0), size(0) {}
+    MessageBox(): deviceId(-1), maxLength(0), length(0) {
+        buffers[0] = NULL;
+        buffers[1] = NULL;
+    }
 
     /** Allocating space for the message box */
-    void reserve(size_t maxLength, int id) {
-        firstBuffer = new GRD<Message>;
-        secondBuffer = new GRD<Message>;
-        firstBuffer.reserve(maxLength, id);
-        secondBuffer.reserve(maxLength, id);
+    void reserve(size_t len, int id) {
+        maxLength = len;
         deviceId = id;
-        size = 0;
+        length = 0;
+        assert(len > 0);
+        assert(id >= 0);
+        CUDA_CHECK(cudaSetDevice(deviceId));
+        CUDA_CHECK(cudaMallocHost(reinterpret_cast<void **>(&buffers[0]),
+                                  len * sizeof(Message),
+                                  cudaHostAllocPortable));
+        CUDA_CHECK(cudaMallocHost(reinterpret_cast<void **>(&buffers[1]),
+                                  len * sizeof(Message),
+                                  cudaHostAllocPortable));
     }
 
-    /** Send the content of this message box to another */
-    void send(MessageBox) {
-
+    /**
+     * Copies the content of a remote message box to this.
+     * We use asynchronized memory copy to hide the memory latency with 
+     * computation.
+     *
+     * Assuming the peer-to-peer access is already enabled.
+     * 
+     * @param other   The message box to copy.
+     * @stream stream The stream to perform this copy within.
+     */
+    void copyMsgs(const MessageBox &other, cudaStream_t stream) {
+        assert(deviceId != other.deviceId);
+        assert(other.length <= maxLength);
+        assert(other.length > 0);
+        length = other.length;
+        CUDA_CHECK(cudaSetDevice(deviceId));
+        CUDA_CHECK(cudaMemcpyAsync(buffers[0],
+                                   other.buffers[0],
+                                   other.length * sizeof(Message),
+                                   cudaMemcpyDefault, stream));
     }
 
-    /** Swap the two buffers. */
-    inline void refresh(void) {
-        GRD<Message> * temp = firstBuffer;
-        firstBuffer = secondBuffer;
-        secondBuffer = temp;
+    /** 
+     * Exchanges two buffers.
+     */
+    inline void exchange() {
+        if (maxLength > 0) {
+            Message * temp = buffers[0];
+            buffers[0] = buffers[1];
+            buffers[1] = temp;
+        }
     }
 
-    ~MessageBox(void) {
-        if (firstBuffer)  delete firstBuffer;
-        if (secondBuffer) delete secondBuffer;
+    /** Destructor */
+    ~MessageBox() {
+        if (maxLength > 0) {
+            CUDA_CHECK(cudaSetDevice(deviceId));
+            CUDA_CHECK(cudaFreeHost(buffers[0]));
+            CUDA_CHECK(cudaFreeHost(buffers[1]));
+        }
     }
-}
-
+};
 
 #endif  // MESSAGE_BOX_H
